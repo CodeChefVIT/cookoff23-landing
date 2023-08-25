57fc89410d4a8439fed824976fd9b20bd4b26643
arduR-O <omaansh@gmail.com>
2023-08-25 16:05:00 +0530

    feat: responsiveness

--- cookoff24-landing/src/pages/index.js
+++ cookoff24-landing/src/pages/index.js
@@ -5,11 +5,13 @@ import { motion } from "framer-motion";
 import { Inter } from "next/font/google";
 import SVGAnimation from "@/components/SVGAnimation";
 import SVGAnimation2 from "@/components/SVGAnimation2";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 const inter = Inter({ subsets: ["latin"] });
 import Link from "next/link";
 
 const index = () => {
+  const isDesktop = useMediaQuery("(min-width: 840px)");
   /* Cursor following div states*/
 
   // I want to make a state to store the current position of cursor
@@ -35,8 +37,13 @@ const index = () => {
       scale: 5,
       mixBlendMode: "difference",
     },
+    notDefaultButBigger: {
+      x: cursorPos.x - 20,
+      y: cursorPos.y - 20,
+      scale: 750,
+      mixBlendMode: "difference",
+    },
   };
-
 
   /*Icon Animation*/
 
@@ -70,6 +77,7 @@ const index = () => {
     };
   }, []);
 
+  
   return (
     <>
       <Head>
@@ -77,13 +85,28 @@ const index = () => {
         <link rel="icon" href="/Favicon.png" />
       </Head>
 
-
       {/* body container */}
-      <div className="h-[100vh] w-[100vw] text-grey flex justify-center justify-items-center">
-
+      <div className="h-[100vh] w-[100vw] text-grey flex justify-center justify-items-center no-selection">
+        {
+          isDesktop &&
+         <motion.div
+          className={`w-1/3 absolute h-screen left-10 break-words justify-items-center ${
+            hasClicked ? "text-[90px]" : "text-9xl"
+          } font-bold text-black`}
+          onMouseEnter={() => {
+            setCurrentVariant("notDefault");
+          }}
+          onMouseLeave={() => {
+            setCurrentVariant("default");
+          }}
+        >
+          {hasClicked
+            ? "LOOK HOW THEY BUTCHERED MY BOY"
+            : "DO NOT DARE DRAG OUR CHEF"}
+        </motion.div>
+}
         {/* main flex */}
         <div className="flex flex-col justify-center justify-items-center gap-36 ">
-
           {/* icon */}
           <div
             className=" flex justify-center relative before:absolute before:bg-dark-grey before:h-56 before:w-56 before:-z-10 before:-bottom-16 before:rounded-full"
@@ -92,9 +115,9 @@ const index = () => {
             ref={eleRef}
           >
             {hasClicked ? (
-              <SVGAnimation2/>
+              <SVGAnimation2 />
             ) : (
-              <SVGAnimation fill={fill} setHasClicked={setHasClicked}/>
+              <SVGAnimation fill={fill} setHasClicked={setHasClicked} />
             )}
           </div>
 
@@ -111,7 +134,7 @@ const index = () => {
             </Link>
           </div>
         </div>
-        
+
         {/* cursor div */}
         <motion.div
           className="bg-orange w-10 h-10 fixed rounded-full left-0 top-0 -z-10"
@@ -119,6 +142,23 @@ const index = () => {
           // animate="default"
           animate={currentVaraint}
         />
+      { isDesktop &&
+       <motion.div
+          className={`w-1/3 absolute h-screen right-10 break-words justify-items-center ${
+            hasClicked ? "text-[95px]" : "text-9xl"
+          } font-bold text-black`}
+          onMouseEnter={() => {
+            setCurrentVariant("notDefault");
+          }}
+          onMouseLeave={() => {
+            setCurrentVariant("default");
+          }}
+        >
+          {hasClicked
+            ? "LOOK HOW THEY BUTCHERED MY BOY"
+            : "DO NOT DARE DRAG OUR CHEF"}
+        </motion.div>
+}
       </div>
     </>
   );
