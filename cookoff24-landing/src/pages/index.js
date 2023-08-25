14d7f6448afeffbb5c5d9a188d1f3501826a5782
arduR-O <omaansh@gmail.com>
2023-08-25 14:56:46 +0530

    feat: button, icon animations

--- cookoff24-landing/src/pages/index.js
+++ cookoff24-landing/src/pages/index.js
@@ -1,16 +1,18 @@
 import React, { useEffect } from "react";
 import Head from "next/head";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { motion } from "framer-motion";
 import { Inter } from "next/font/google";
 import SVGAnimation from "@/components/SVGAnimation";
+import SVGAnimation2 from "@/components/SVGAnimation2";
 
 const inter = Inter({ subsets: ["latin"] });
 import Link from "next/link";
 
 const index = () => {
-  /* states and stuff for the cursor stuff */
-  /* I want to make a state to store the current position of cursor */
+  /* Cursor following div states*/
+
+  // I want to make a state to store the current position of cursor
   const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 });
   useEffect(() => {
     const mouseMove = (e) => {
@@ -18,14 +20,55 @@ const index = () => {
     };
     window.addEventListener("mousemove", mouseMove);
 
-    return ()=>{window.removeEventListener("mousemove", mouseMove)};
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
   }, []);
   const cursorVariants = {
     default: {
       x: cursorPos.x - 20,
-      y: cursorPos.y - 20
+      y: cursorPos.y - 20,
+    },
+    notDefault: {
+      x: cursorPos.x - 20,
+      y: cursorPos.y - 20,
+      scale: 5,
+      mixBlendMode: "difference",
     },
   };
+
+
+  /*Icon Animation*/
+
+  const [fill, setFill] = useState("#B7AB98");
+  const [currentVaraint, setCurrentVariant] = useState("default");
+  // const [curDem, setCurDem] = useState({w:10,h:10});
+  const onIconEnter = () => {
+    setFill("#FFFFFF");
+    // setCurDem({w:20, h:20})
+    setCurrentVariant("notDefault");
+  };
+  const onIconLeave = () => {
+    setFill("#B7AB98");
+    // setCurDem({w:10, h:10})
+    setCurrentVariant("default");
+  };
+
+  /*Chef Drag Animation */
+  const [hasClicked, setHasClicked] = useState(false);
+  const eleRef = useRef(null);
+  useEffect(() => {
+    const clicked = (e) => {
+      if (eleRef.current === e.target) {
+        setHasClicked(true);
+      }
+    };
+    window.addEventListener("click", clicked);
+
+    return () => {
+      window.removeEventListener("click", clicked);
+    };
+  }, []);
 
   return (
     <>
@@ -33,30 +76,48 @@ const index = () => {
         <title>Cook-off 8.0</title>
         <link rel="icon" href="/Favicon.png" />
       </Head>
+
+
       {/* body container */}
       <div className="h-[100vh] w-[100vw] text-grey flex justify-center justify-items-center">
+
         {/* main flex */}
-        <div className="flex flex-col justify-center justify-items-center gap-36">
+        <div className="flex flex-col justify-center justify-items-center gap-36 ">
+
           {/* icon */}
-          <div className=" flex justify-center relative before:absolute before:bg-dark-grey before:h-56 before:w-56 before:-z-10 before:-bottom-16 before:rounded-full">
-            <SVGAnimation cursorPos = {cursorPos}/>
+          <div
+            className=" flex justify-center relative before:absolute before:bg-dark-grey before:h-56 before:w-56 before:-z-10 before:-bottom-16 before:rounded-full"
+            onMouseEnter={onIconEnter}
+            onMouseLeave={onIconLeave}
+            ref={eleRef}
+          >
+            {hasClicked ? (
+              <SVGAnimation2/>
+            ) : (
+              <SVGAnimation fill={fill} setHasClicked={setHasClicked}/>
+            )}
           </div>
+
           {/* button */}
-          <div className="flex justify-center">
+          <div className="flex justify-center ">
             <Link href={"/landing"}>
-              <button
-                className={`text-grey text-xl rounded-full border-2 border-grey w-56 py-3 ${inter.className}`}
+              <motion.button
+                className={`text-grey text-xl rounded-full border-2 border-grey w-56 py-3 ${inter.className}  hover:bg-orange hover:border-4 hover:border-dark-grey hover:text-black hover:font-bold`}
+                whileTap={{ scale: 0.9 }}
+                whileHover={{ scale: 1.2 }}
               >
                 START
-              </button>
+              </motion.button>
             </Link>
           </div>
         </div>
+        
         {/* cursor div */}
         <motion.div
-          className="bg-orange w-10 h-10 fixed rounded-full left-0 top-0"
+          className="bg-orange w-10 h-10 fixed rounded-full left-0 top-0 -z-10"
           variants={cursorVariants}
-          animate="default"
+          // animate="default"
+          animate={currentVaraint}
         />
       </div>
     </>
