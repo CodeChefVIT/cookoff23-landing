749245d6a56141f39b4f59ea2860936361ce145e
Shivd131 <shivdeshpande131@gmail.com>
2023-08-26 09:08:18 +0530

    feat: responsiveness and max-width media params in tailwind config

--- cookoff24-landing/src/components/About.js
+++ cookoff24-landing/src/components/About.js
@@ -33,7 +33,7 @@ const About = () => {
     <div className="h-[100vh] w-[100vw] relative  cursor-default">
       <div className="absolute body flex flex-col justify-center items-left gap-[8rem] py-16 px-36 h-[100vh]   text-grey">
         <p className="text-lg font-normal tracking-[0.15rem] ">ABOUT EVENT</p>
-        <p className="text-left text-4xl tracking-wide font-bold leading-relaxed">
+        <p className="text-left text-4xl tracking-wide font-bold leading-relaxed  sm:text-2xl sm:tracking-wide sm:font-semibold">
           Cook off is <span className="text-orange">CodeChef VIT</span>’s
           flagship competitive coding event that tests the coding skills of tech
           enthusiasts across the country. The event provides a platform for
--- cookoff24-landing/tailwind.config.js
+++ cookoff24-landing/tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -6,18 +5,23 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      '2xl': {'max': '1535px'},
+      'xl': {'max': '1279px'},
+      'lg': {'max': '1023px'},
+      'md': {'max': '767px'},
+      'sm': {'max': '639px'},
+    },
     extend: {
       colors: {
         orange: "#EB5939",
         grey: "#B7AB98",
         "dark-grey": "#212121",
         black: "#0D0D0D",
-        
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
