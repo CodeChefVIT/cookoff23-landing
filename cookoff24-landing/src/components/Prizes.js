71b03ae6d737736e385b5a4872b14cf236426dd0
Shivd131 <shivdeshpande131@gmail.com>
2023-08-26 09:32:59 +0530

    update: Codechef logo

--- cookoff24-landing/src/components/Prizes.js
+++ cookoff24-landing/src/components/Prizes.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Prizes = () => {
   return (
-    <div className='h-[100vh] w-[100vw] text-grey'>Prizes</div>
+    <div className='h-[100vh] w-[100vw] text-grey flex flex-col items-start py-16'>
+      <p className='text-lg font-normal tracking-[0.15rem] px-36'>PRIZES</p>
+      <div>
+        <div className='w-[100vw] flex justify-between'>
+          <p className='px-36'>FIRST</p>
+          <p className='px-36'>SDFGHJKJHGFD</p>
+        </div>
+        <div>Second</div>
+        <div>THird</div>
+        <div>Fresher</div>
+        <div>Women</div>
+      </div>
+    </div>
   )
 }
 
