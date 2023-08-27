6cdaa25b3fd52bb116fc10cea7408e1a660be561
Gaurav Choudhary <gloonaich@gmail.com>
2023-08-27 11:41:33 +0530

    fix

--- cookoff24-landing/src/components/Timeline.js
+++ cookoff24-landing/src/components/Timeline.js
@@ -29,9 +29,7 @@ const Timeline = () => {
   }, []);
   return (
     <div className="h-[100vh] w-[100vw] text-grey text-9xl font-bold  mx-32 flex flex-col gap-64 pt-10">
-      <div className="uppercase text-[16px] md:text-[10px] mx-[13%] md:mx-[10%] text-[#B7AB98] mt-32 mb-10 tracking-[7px]">
-        Timeline
-      </div>
+      <div className="text-lg font-normal tracking-[7px]">TIMELINE</div>
       <div className="flex gap-96 ml-16 sm:flex-col md:gap-16 justify-items-center">
         <div className="flex flex-col justify-center">
           <div className="flex flex-col justify-center text-center">
