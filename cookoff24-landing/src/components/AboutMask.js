7b2d77bbca8c48d7650882d09b370b326fda5ef1
arduR-O <omaansh@gmail.com>
2023-08-27 21:07:14 +0530

    fix: seriously minor fix

--- cookoff24-landing/src/components/AboutMask.js
+++ cookoff24-landing/src/components/AboutMask.js
@@ -25,13 +25,13 @@ const AboutMask = () => {
         onMouseLeave={() => {
           setIsHovered(false);
         }}
-        className="body mx-[13%] md:mx-[10%] flex flex-col justify-center items-stretch gap-[8rem] py-16 h-[100vh] text-grey"
+        className="body mx-[13%] md:mx-[10%] flex flex-col justify-center items-stretch gap-[8rem] py-16 h-[100vh] text-dark-grey"
       >
         <p
           id="proxima"
           className="text-justify text-4xl tracking-wide font-bold leading-relaxed lg:text-2xl lg:tracking-wide lg:font-semibold sm:text-2xl sm:tracking-wide sm:font-semibold"
         >
-          Cook off is <span className="text-black">CodeChef VIT</span>&apos;s
+          Cook off is <span className="text-white">CodeChef VIT</span>&apos;s
           flagship competitive coding event that tests the coding skills of tech
           participants to develop their competitive coding skills and test their
           abilities. Cook Off 7.0 will be the seventh edition through which we
