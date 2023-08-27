d6874882349bf34aef3c61050f0aa9bec38a6192
arduR-O <omaansh@gmail.com>
2023-08-27 12:46:56 +0530

    feat:timeline

--- cookoff24-landing/src/components/Timeline.js
+++ cookoff24-landing/src/components/Timeline.js
@@ -6,14 +6,19 @@ const Timeline = () => {
     { time: "12:00 PM", event: "Placeholder" },
     { time: "2:00 PM", event: "Event Commences" },
   ];
+  console.log(100/events.length);
   return (
     <div className="h-[100vh]">
-      <div className="text-lg font-normal tracking-[7px]">TIMELINE</div>
-      <div className="flex flex-col">
+      <div className="text-lg font-normal tracking-[7px] ml-40 text-grey">TIMELINE</div>
+      <div className="flex flex-col h-[90%]">
         {events.map((e) => {
-          return (<div className="flex justify-between ml-20">
-            <div>{e.time}</div>
-            <div>{e.event}</div>
+          return (<div 
+            // className={`flex ml-40 h-[20%]`}
+            className={`flex ml-40 h-[${100/events.length}%] border-b-2 border-grey gap-44 text-grey text-3xl font-semibold`}
+          
+          >
+            <div className=" flex flex-col justify-center">{e.time}</div>
+            <div className="flex flex-col justify-center">{e.event}</div>
           </div>);
         })}
       </div>
